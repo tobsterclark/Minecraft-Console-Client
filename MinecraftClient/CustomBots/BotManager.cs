7f22ae9c730479603372ef4e6ab077167454cbee
tobsterclark <me@tobyclark.dev>
2023-08-21 15:30:52 +1000

    Working on new command structure

--- MinecraftClient/CustomBots/BotManager.cs
+++ MinecraftClient/CustomBots/BotManager.cs
@@ -5,6 +5,7 @@ using MinecraftClient.CustomBots.BotUtils;
 using System.Linq;
 using MinecraftClient.Commands;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace MinecraftClient.CustomBots
 {
@@ -29,6 +30,7 @@ namespace MinecraftClient.CustomBots
             LogToConsole("AFK bot manager initialized!");
         }
 
+
         // This is a function that will be run when we get a chat message from a server
         // In this example it just detects the type of the message and prints it out
         public override void GetText(string text)
@@ -36,11 +38,17 @@ namespace MinecraftClient.CustomBots
             ChatUtils.ChatType type = ChatUtils.ChatType.none;
             string player = "";
             string message = "";
+            bool commandLocked = false;
 
             ChatUtils.CleanText(text, ref type, ref player, ref message);
 
-            if (type == ChatUtils.ChatType.general && !message.StartsWith("~")) { return; }
-            else if (type == ChatUtils.ChatType.teleport && BotOwners.Contains(player))
+            if (type == ChatUtils.ChatType.general && !message.StartsWith("~")) { 
+                return; 
+            } else if (type == ChatUtils.ChatType.general && message.StartsWith("~"))
+            {
+                printing.PrintError("Send commands to the bot via personal messages", type, player);
+                return;
+            } else if (type == ChatUtils.ChatType.teleport && BotOwners.Contains(player))
             {
                 if (runningBot != null)
                 {
@@ -56,52 +64,71 @@ namespace MinecraftClient.CustomBots
 
             LogToConsole($"Recieved {message} from {player} in a {type} message.");
 
-            if (locked && lockedTime < DateTime.UtcNow.AddHours(3) && !BotOwners.Contains(player)) 
+            CheckLocked(player, ref commandLocked);
+            HandleCommand(message, player, type);
+        }
+
+        void HandleCommand(string command, string player, ChatUtils.ChatType type) 
+        {
+            if (command.StartsWith("start-"))
             {
-                printing.PrintError("Bot is currently locked at afk position", type, player);
-                return;
-            } else if (locked && lockedTime >= DateTime.UtcNow.AddHours(3)) 
+                string botName = command.Split("-")[1];
+                CreateBot(botName, player, type);
+            }
+            else if (command == "stop")
+            {
+                if (runningBot != null) { StopBot(runningBot, player, type); }
+            }
+            else if (command == "quit" && BotOwners.Contains(player))
+            {
+                PerformInternalCommand("quit");
+            }
+            else if (command == "lock" && BotOwners.Contains(player))
+            {
+                locked = true;
+                lockedTime = DateTime.UtcNow;
+            }
+            else if (command == "unlock" && BotOwners.Contains(player))
             {
                 locked = false;
                 lockedTime = null;
             }
-
-            if (message.StartsWith("start-"))
-            {
-                string botName = message.Split("-")[1];
-                CreateBot(botName, player, type);
-            } else if (message == "stop")
-            {
-                if (runningBot != null) { StopBot(runningBot, player, type); }
-            } else if (message == "quit" && BotOwners.Contains(player))
-            {
-                PerformInternalCommand("quit");
-            } else if (message == "lock" && BotOwners.Contains(player))
-            {
-                locked = true;
-                lockedTime = DateTime.UtcNow;
-            } else if (message == "unlock" && BotOwners.Contains(player))
-            {
-                locked = false;
-                lockedTime = null;
-            } else if (message == "farms")
+            else if (command == "farms")
             {
                 List<string> botList = new();
 
-                foreach(AFKBot bot in AvailableBots)
+                foreach (AFKBot bot in AvailableBots)
                 {
                     botList.Add(bot.Name);
                 }
                 LogToConsole(string.Join(", ", botList));
 
                 printing.PrintInfo(string.Join(", ", botList), type, player);
-            } else
+            }
+            else
             {
                 printing.PrintError("Command not known", type, player);
             }
         }
 
-        void CreateBot(string botName, string player, ChatUtils.ChatType type)
+        void CheckLocked(string player, ref bool commandLocked)
+        {
+            if (locked && lockedTime < DateTime.UtcNow.AddHours(3) && BotOwners.Contains(player))
+            {
+                commandLocked = false;
+            }
+            else if (locked && lockedTime >= DateTime.UtcNow.AddHours(3))
+            {
+                locked = false;
+                lockedTime = null;
+                commandLocked = false;
+            } else
+            {
+                commandLocked = locked;
+            }
+        }
+
+        void CreateBot(string botName, string player)
         {
             // Stop running bot
             if (runningBot != null) { StopBot(runningBot, player, type); }
@@ -115,22 +142,23 @@ namespace MinecraftClient.CustomBots
             { 
                 if (bot.Name.ToLower() == botName.ToLower())
                 {
-                    printing.PrintSuccess($"Starting AFK session at {bot.Name}", type, player);
+                    printing.PrintSuccess($"Starting AFK session at {bot.Name}", ChatUtils.ChatType.personal, player);
                     //CollectResources(bot.NeededItems)
                     BotLoad(bot);
                     bot.Start();
                     runningBot = bot;
+                    return;
                 }
             }
         }
 
-        void StopBot(AFKBot bot, string player, ChatUtils.ChatType type) 
+        void StopBot(AFKBot bot, string player) 
         {
             if (runningBot != bot) { return; }
 
             bot.Stop();
 
-            printing.PrintInfo($"Stopping {bot.Name}", type, player);
+            printing.PrintInfo($"Stopping {bot.Name}", ChatUtils.ChatType.personal, player);
             runningBot = null;
         }
     }
---
+++ MinecraftClient/CustomBots/BotUtils/Command.cs
@@ -0,0 +1,64 @@
+﻿using MinecraftClient.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftClient.CustomBots.BotUtils
+{
+    public interface CommandTemplate
+    {
+        static string id = String.Empty;
+        static abstract void Help(string player);
+        static abstract void Run(string command, string player, AFKBot[] availableBots, ref bool locked, ref DateTime? lockTime, Action<string, string> CreateBot, Action<string, string> StartBot, Action<string, string> StopBot);
+        static abstract bool Check(string command);
+    }
+
+    public class CommandHelp : CommandTemplate
+    {
+        private CommandHelp() { }
+        private static ChatUtils printing = new();
+        public const string id = "help";
+
+        public static bool Check(string command)
+        {
+            if (string.IsNullOrEmpty(command)) return false;
+            else if (command == "help") return true;
+            else return false;
+        }
+
+        public static void Help(string player)
+        {
+            printing.PrintInfo("you really need help on how to use the help command?", ChatUtils.ChatType.personal, player);
+        }
+
+        public static void Run(string command, string player, AFKBot[] availableBots, ref bool locked, ref DateTime? lockTime, Action<string, string> CreateBot, Action<string, string> StartBot, Action<string, string> StopBot)
+        {
+            printing.PrintInfo("Available commands: \n - help\n - start-{bot}\n - create\n - stop\n - quit\n - lock \n - bots", ChatUtils.ChatType.personal, player);
+        }
+    }
+
+    public class CommandStart : CommandTemplate
+    {
+        private CommandStart() { }
+        private static ChatUtils printing = new();
+        public const string id = "start_bot";
+
+        public static bool Check(string command)
+        {
+            return command.StartsWith("start-");
+        }
+
+        public static void Help(string player)
+        {
+            printing.PrintInfo("Usage: start-{bot}\nTo see available bots use the `bots` command", ChatUtils.ChatType.personal, player);
+        }
+
+        public static void Run(string command, string player, AFKBot[] availableBots, ref bool locked, ref DateTime? lockTime, Action<string, string> CreateBot, Action<string, string> StartBot, Action<string, string> StopBot)
+        {
+            string botName = command.Split("-")[1];
+            StartBot(botName, player);
+        }
+    }
+}
